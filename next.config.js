fd132f780533ac39f70ed725c7ee1a4ca389e7a8
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2022-12-28 16:26:19 +0700

    edit rewrites in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => [
-    {
-      source: '/gallery',
-      destination: 'https://gallery.phatsanphon.com/'
-    }
-  ]
 }
 
 module.exports = nextConfig
